bedf69325e7a151a0f204d9a5093e4278cd92306
sreetej1983 <sreetejdepa@gmail.com>
2020-09-24 19:00:51 +0530

    Commit it

--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+//We are conmiting
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
